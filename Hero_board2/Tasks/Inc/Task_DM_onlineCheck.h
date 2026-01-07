116429bb11d60b4f7464ecf64181dc819cc2c79a
BeiziStar <2936567979@qq.com>
2026-01-07 20:26:04 +0800

    feat: 添加DM电机掉线检测任务
    实现DM电机掉线检测功能，当检测到电机掉线时自动重新使能
    新增Task_DM_onlineCheck.c/h文件
    更新项目配置文件和编译依赖

---
+++ Hero_board2/Tasks/Inc/Task_DM_onlineCheck.h
@@ -0,0 +1,15 @@
+/**
+ * @file Task_DM_onlineCheck.h
+ * @author SJW
+ * @brief   检测DM电机掉线并使能
+ * @version 0.1
+ * @date 2025-08-12
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "DM_Motor.h"
+#include "Extern_Handles.h"
+
+void DM_onlineCheck(void const *argument);
---
+++ Hero_board2/Tasks/Src/Task_DM_onlineCheck.c
@@ -0,0 +1,31 @@
+/**
+ * @file Task_DM_onlineCheck.c
+ * @author SJW
+ * @brief 检测DM电机掉线并使能
+ * @version 0.1
+ * @date 2025-08-12
+ *
+ * @copyright Copyright (c)
+ *
+ */
+
+#include "Task_DM_onlineCheck.h"
+
+uint16_t DM_Frame = 0;
+
+void DM_onlineCheck(void const *argument)
+{
+
+    portTickType xLastWakeTime;
+    xLastWakeTime = xTaskGetTickCount();
+    const TickType_t TimeIncrement = pdMS_TO_TICKS(1);
+    for (;;)
+    {
+        if (DM_Frame == DM_Array[0].InfoUpdateFrame)
+        {
+            DM_Fun.DM_Enable(&hcan1, DM_SENDID_1);
+        }
+        DM_Frame = DM_Array[0].InfoUpdateFrame;
+        vTaskDelayUntil(&xLastWakeTime, TimeIncrement);
+    }
+}
